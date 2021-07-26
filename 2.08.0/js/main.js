add3e9181ed7fa14eca87ac3eba6c38b404bb5d5
Bharath <bharathchandra630@gmail.com>
2021-07-26 18:35:26 +0530

    dynamically updating y scales

--- 2.08.0/js/main.js
+++ 2.08.0/js/main.js
@@ -4,14 +4,19 @@
 *    2.8 - Activity: Your first visualization!
 */
 
+const svg = d3.select('#chart-area')
+.append("svg").attr('width',500).attr('height',500)
+
+
 d3.json('data/buildings.json')
 .then(data => {
     let gap = 10;
     data.forEach(d => {
         d.height = Number(d.height)
     })
-    const svg = d3.select('#chart-area')
-.append("svg").attr('width',500).attr('height',500)
+const y = d3.scaleLinear()
+.domain([0,887])
+.range([0,500])
 
 const bars = svg
 .selectAll('rect')
@@ -21,7 +26,7 @@ bars.enter().append('rect')
 .attr('x',(d,i)=> (i*60))
 .attr('y',0)
 .attr('width',40)
-.attr('height',(d,i)=>(d.height))
+.attr('height',(d,i)=>y(d.height))
 .attr('fill','grey')
 })
 
